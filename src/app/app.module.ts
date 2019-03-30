55bbe3450681d217b98c95de770f5a3553b5e7cb
unknown <devethancraig@gmail.com>
2019-03-29 20:07:10 -0600

    Added Static Input && Btn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './components/title/title.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent
+    TitleComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule
